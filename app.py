5b78187c9577fbf7f22d6fecb23bdc27f4f23d34
Alexanderms36 <alexanderms3646@gmail.com>
2025-01-28 23:30:10 +0500

    feat(app): try except added

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ classes = ['goose', 'turkey', 'chicken', 'rooster', 'ostrich', 'duck', 'chick']
 model = CNN()
 model.load_state_dict(torch.load("model_weights.pth"))
 
-
 while True:
         print("==============================================")
         print("Type a picture path (or type 'quit' to close)")
@@ -15,17 +14,19 @@ while True:
         if (img_path == "quit"):
                 print("App closed.")
                 break
+        try:
+                image = load_dataset(img_path, is_dataset=False)
 
-        image = load_dataset(img_path, is_dataset=False)
+                model.eval()
+                with torch.no_grad():
+                        outputs = model(image)
+                        _, pred_class = torch.max(outputs, 1)
+                        probabilities = torch.nn.functional.softmax(outputs, dim=1).view(-1)
 
-        model.eval()
-        with torch.no_grad():
-                outputs = model(image)
-                _, pred_class = torch.max(outputs, 1)
-                probabilities = torch.nn.functional.softmax(outputs, dim=1).view(-1)
+                pred_index = pred_class.item()
+                pred_prob = probabilities[pred_index] * 100
 
-        pred_index = pred_class.item()
-        pred_prob = probabilities[pred_index] * 100
-
-        print(f"It's a {classes[pred_index]}")
-        print(f"Probability: {pred_prob:.2f}%")
+                print(f"It's a {classes[pred_index]}")
+                print(f"Probability: {pred_prob:.2f}%")
+        except:
+                print("image path is incorrect")
