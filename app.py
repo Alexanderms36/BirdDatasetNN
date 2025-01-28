24767f8f158210c82606c7cd8d064b3a916a4045
Alexanderms36 <alexanderms3646@gmail.com>
2025-01-28 18:59:08 +0500

    refactor: app refactored

--- app.py
+++ app.py
@@ -7,14 +7,25 @@ classes = ['goose', 'turkey', 'chicken', 'rooster', 'ostrich', 'duck', 'chick']
 model = CNN()
 model.load_state_dict(torch.load("model_weights.pth"))
 
-print("Type a picture path")
-img_path = input()
 
-image = load_dataset(img_path, is_dataset=False)
+while True:
+        print("==============================================")
+        print("Type a picture path (or type 'quit' to close)")
+        img_path = input()
+        if (img_path == "quit"):
+                print("App closed.")
+                break
 
-model.eval()
-with torch.no_grad():
-        outputs = model(image)
-        _, predicted_class = torch.max(outputs, 1)
+        image = load_dataset(img_path, is_dataset=False)
 
-print(f"It's a {classes[predicted_class.item()]}")
+        model.eval()
+        with torch.no_grad():
+                outputs = model(image)
+                _, pred_class = torch.max(outputs, 1)
+                probabilities = torch.nn.functional.softmax(outputs, dim=1).view(-1)
+
+        pred_index = pred_class.item()
+        pred_prob = probabilities[pred_index] * 100
+
+        print(f"It's a {classes[pred_index]}")
+        print(f"Probability: {pred_prob:.2f}%")
